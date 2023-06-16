88f1ddc5facd5c8bfa0eba39231308239b823835
Letya <100812393+Letyakunai@users.noreply.github.com>
2023-06-17 05:28:06 +0800

    Big Update

--- Assets/Scripts/PlaceObjectOnPlane.cs
+++ Assets/Scripts/PlaceObjectOnPlane.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
 
+[RequireComponent(typeof(ARRaycastManager))]
 public class PlaceObjectOnPlane : MonoBehaviour
 {
+
+    [SerializeField] private GameObject prefab;
+    private GameObject spawnedObject;
+
+    private ARRaycastManager racaster;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
     // Start is called before the first frame update
     void Start()
     {
-
+        racaster = GetComponent<ARRaycastManager>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void OnPlaceObject(InputValue value)
     {
-        
+        // Get the screen touch position
+        Vector2 touchPosition = value.Get<Vector2>();
+        // Perform a raycast from the touchPosition into the 3D scene to look into
+        if(racaster.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            // Get the hit point (pose) on the plane
+            Pose hitPoint = hits[0].pose;
+            // Is this the first time we will place an object?
+            if(spawnedObject != null)
+            {
+                //Instantiate our own prefab
+                spawnedObject = Instantiate(prefab, hitPoint.position, hitPoint.rotation);
+            }
+            else
+            {
+                //if the object is SqlAlreadyFilledException existing. We move it.
+
+                spawnedObject.transform.SetPositionAndRotation(hitPoint.position,hitPoint.rotation);
+            }
+
+        }
     }
 }
